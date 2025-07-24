60c21ae49a4153e54f16bf466efa974f1a27d897
Aileen127 <fleuraileen394@gmail.com>
2025-07-24 09:53:14 -0600

    commit fin de clase

--- menu_interactivo.py
+++ menu_interactivo.py
@@ -1,10 +1,42 @@
-# Menu interactivo
-numeros = []
+
+
 # Definicion de funciones
-def suma_total(a, b):
-    return a + b
 
-def promedio(a, b):
-    a + b / len(numeros)
+def ingreso_numeros():
+    numero = int(input("Ingresa la cantidad de números que deseas evaluar (digitos)"))
 
-def
+    if numero > 0:
+        def suma_total():
+            for n in numeros:
+                suma = n + n
+                print(f"EL total es de: {suma}")
+
+
+
+
+    else:
+        print("Ingresa un dato valido por favor")
+
+
+
+
+
+# Menu interactivo
+while True:
+    print("\n Bienvenido al menú")
+    print("1. Ingresa n números para mostrar \n 1. Suma total \n 2. Promedio "
+          "\n Cantidad de negativos y positivos")
+    print("2. Calcular el área de un triángulo")
+    print("3. Verificar si un número es par o impar")
+    print("4. Calcular el promedio de n calificaciones")
+    print("5. INgresar n números y mostrar el mayor y el menor")
+    print("6. Salir del programa")
+
+    option = input("Ingresa una opción por favor (1 - 6)")
+
+    match option:
+        case "1":
+            ingreso_numeros()
+
+        case "6":
+            break
