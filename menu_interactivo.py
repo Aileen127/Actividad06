a1b68dc1df735d9407c2c128bd485881a3ef2881
Aileen127 <fleuraileen394@gmail.com>
2025-07-24 17:51:28 -0600

    commit 3, no daba la suma y sigue sin dar pero cambio el codigo

--- menu_interactivo.py
+++ menu_interactivo.py
@@ -2,19 +2,29 @@
 
 # Definicion de funciones
 
-def ingreso_numeros():
-    numeros = []
-    numero = int(input("Ingresa la cantidad de números que deseas evaluar (digitos)"))
+def suma_total(numero):
+    suma = 0
+    for n in numeros:
+        if numero != 0:
+            suma += n
+        return suma
 
-    if numero > 0:
-        def suma_total(numero):
-            suma = 0
-            for n in numeros:
-                suma += n
-                print(f"EL total es de: {suma}")
+def promedio(numero):
+        return sum(numero) / len(numero)
 
-        def promedio(numero):
-            return sum(numeros) / len(numeros)
+def cantidad_positivos_negativos(numero):
+    positivo=0
+    negativo=0
+    for n in range(numero):
+        if n > 0:
+            positivo += 1
+            print(f"Hay {positivo}  números positivos")
+
+        elif n < 0:
+            negativo += 1
+            print(f"Hay {negativo} números negativos")
+
+
 
 
 
@@ -34,14 +44,23 @@ while True:
     print("2. Calcular el área de un triángulo")
     print("3. Verificar si un número es par o impar")
     print("4. Calcular el promedio de n calificaciones")
-    print("5. INgresar n números y mostrar el mayor y el menor")
+    print("5. Ingresar n números y mostrar el mayor y el menor")
     print("6. Salir del programa")
 
-    option = input("Ingresa una opción por favor (1 - 6)")
+    option = input("Ingresa una opción por favor (1 - 6): ")
 
     match option:
         case "1":
-            ingreso_numeros()
+            numeros = []
+            while True:
+                numero = int(input("Ingresa los números que deseas evaluar, ingresa 0 si ya NO deseas ingresar más números: "))
+                if numero != 0:
+                    numeros.append(numero)
+                elif numero == 0:
+                    break
+            resultado = suma_total(numeros)
+            print(f"La suma total es de: {suma_total(numeros)}")
+
 
         case "6":
             break
